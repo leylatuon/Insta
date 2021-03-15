d0489a0b6b9baeb95bcc765755f1ea2850ba52e3
leylatuon <leylatuon@users.noreply.github.com>
2021-03-14 23:56:43 -0400

    done

--- Insta/AppDelegate.swift
+++ Insta/AppDelegate.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "HoUDkSfLxUa01mYJ38fwGdTN5cmfEngZTDDYHlkl" // <- UPDATE
+                $0.clientKey = "gV8okRLOUICHem78ofbEvASAAnxGDuDGS6FqrQZm" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
         return true
     }
 
@@ -31,6 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    
 
 }
 
